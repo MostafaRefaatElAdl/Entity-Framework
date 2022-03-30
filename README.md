74a917b3fdaf8e838af75f0b1a608d739ebd6a00
Mostafa Refaat <mostafarefaat.eladl@gmail.com>
2022-03-30 17:30:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Warehouse
+
+## Linq & EF Project with C#, windows forms and Microsoft SQL server
+
+### features
+- manage Warehouse
+- manage Product
+- manage Providers
+- manage Customers
+- manage Supplying Orders
+- manage payment Orders
+- keep tracking of Products movement between Warehouses
+- Reports
+
+### Installation tools
+- SQL Server 2021
+- Visual Studio 2022
+
+### ERD
+
+![EntityDesignerDiagram](./EntityDesignerDiagram.png)
---
+++ warehouseManager/warehouse manager/Forms/Home.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace warehouse_manager
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.btnSupplyingOrders = new System.Windows.Forms.Button();
+            this.btnPaymentOrders = new System.Windows.Forms.Button();
+            this.btnNewPaymentOrder = new System.Windows.Forms.Button();
+            this.btnNewSupplyingOrder = new System.Windows.Forms.Button();
+            this.btnExchangeOrders = new System.Windows.Forms.Button();
+            this.btnNewExchangeOrder = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnViewWarehouses = new System.Windows.Forms.Button();
+            this.btnViewProducts = new System.Windows.Forms.Button();
+            this.btnViewUnits = new System.Windows.Forms.Button();
+            this.btnViewProviders = new System.Windows.Forms.Button();
+            this.btnViewCustomers = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSupplyingOrders
+            // 
+            resources.ApplyResources(this.btnSupplyingOrders, "btnSupplyingOrders");
+            this.btnSupplyingOrders.Name = "btnSupplyingOrders";
+            this.btnSupplyingOrders.UseVisualStyleBackColor = true;
+            this.btnSupplyingOrders.Click += new System.EventHandler(this.btnSupplyingOrders_Click);
+            // 
+            // btnPaymentOrders
+            // 
+            resources.ApplyResources(this.btnPaymentOrders, "btnPaymentOrders");
+            this.btnPaymentOrders.Name = "btnPaymentOrders";
+            this.btnPaymentOrders.UseVisualStyleBackColor = true;
+            this.btnPaymentOrders.Click += new System.EventHandler(this.btnPaymentOrders_Click);
+            // 
+            // btnNewPaymentOrder
+            // 
+            resources.ApplyResources(this.btnNewPaymentOrder, "btnNewPaymentOrder");
+            this.btnNewPaymentOrder.Name = "btnNewPaymentOrder";
+            this.btnNewPaymentOrder.UseVisualStyleBackColor = true;
+            this.btnNewPaymentOrder.Click += new System.EventHandler(this.btnNewPaymentOrder_Click);
+            // 
+            // btnNewSupplyingOrder
+            // 
+            resources.ApplyResources(this.btnNewSupplyingOrder, "btnNewSupplyingOrder");
+            this.btnNewSupplyingOrder.Name = "btnNewSupplyingOrder";
+            this.btnNewSupplyingOrder.UseVisualStyleBackColor = true;
+            this.btnNewSupplyingOrder.Click += new System.EventHandler(this.btnNewSupplyingOrder_Click);
+            // 
+            // btnExchangeOrders
+            // 
+            resources.ApplyResources(this.btnExchangeOrders, "btnExchangeOrders");
+            this.btnExchangeOrders.Name = "btnExchangeOrders";
+            this.btnExchangeOrders.UseVisualStyleBackColor = true;
+            this.btnExchangeOrders.Click += new System.EventHandler(this.btnExchangeOrders_Click);
+            // 
+            // btnNewExchangeOrder
+            // 
+            resources.ApplyResources(this.btnNewExchangeOrder, "btnNewExchangeOrder");
+            this.btnNewExchangeOrder.Name = "btnNewExchangeOrder";
+            this.btnNewExchangeOrder.UseVisualStyleBackColor = true;
+            this.btnNewExchangeOrder.Click += new System.EventHandler(this.btnNewExchangeOrder_Click);
+            // 
+            // btnReports
+            // 
+            resources.ApplyResources(this.btnReports, "btnReports");
+            this.btnReports.Name = "btnReports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSupplyingOrders);
+            this.groupBox2.Controls.Add(this.btnPaymentOrders);
+            this.groupBox2.Controls.Add(this.btnExchangeOrders);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // btnViewWarehouses
+            // 
+            resources.ApplyResources(this.btnViewWarehouses, "btnViewWarehouses");
+            this.btnViewWarehouses.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnViewWarehouses.Name = "btnViewWarehouses";
+            this.btnViewWarehouses.UseVisualStyleBackColor = true;
+            this.btnViewWarehouses.Click += new System.EventHandler(this.btnViewWarehouses_Click);
+            // 
+            // btnViewProducts
+            // 
+            resources.ApplyResources(this.btnViewProducts, "btnViewProducts");
+            this.btnViewProducts.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnViewProducts.Name = "btnViewProducts";
+            this.btnViewProducts.UseVisualStyleBackColor = true;
+            this.btnViewProducts.Click += new System.EventHandler(this.btnViewProducts_Click);
+            // 
+            // btnViewUnits
+            // 
+            resources.ApplyResources(this.btnViewUnits, "btnViewUnits");
+            this.btnViewUnits.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnViewUnits.Name = "btnViewUnits";
+            this.btnViewUnits.UseVisualStyleBackColor = true;
+            this.btnViewUnits.Click += new System.EventHandler(this.btnViewUnits_Click);
+            // 
+            // btnViewProviders
+            // 
+            resources.ApplyResources(this.btnViewProviders, "btnViewProviders");
+            this.btnViewProviders.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnViewProviders.Name = "btnViewProviders";
+            this.btnViewProviders.UseVisualStyleBackColor = true;
+            this.btnViewProviders.Click += new System.EventHandler(this.btnViewProviders_Click);
+            // 
+            // btnViewCustomers
+            // 
+            resources.ApplyResources(this.btnViewCustomers, "btnViewCustomers");
+            this.btnViewCustomers.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnViewCustomers.Name = "btnViewCustomers";
+            this.btnViewCustomers.UseVisualStyleBackColor = true;
+            this.btnViewCustomers.Click += new System.EventHandler(this.btnViewCustomers_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnViewCustomers);
+            this.groupBox3.Controls.Add(this.btnViewUnits);
+            this.groupBox3.Controls.Add(this.btnViewProviders);
+            this.groupBox3.Controls.Add(this.btnViewProducts);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // Home
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnReports);
+            this.Controls.Add(this.btnNewExchangeOrder);
+            this.Controls.Add(this.btnNewPaymentOrder);
+            this.Controls.Add(this.btnViewWarehouses);
+            this.Controls.Add(this.btnNewSupplyingOrder);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Home";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSupplyingOrders;
+        private System.Windows.Forms.Button btnPaymentOrders;
+        private System.Windows.Forms.Button btnNewPaymentOrder;
+        private System.Windows.Forms.Button btnNewSupplyingOrder;
+        private System.Windows.Forms.Button btnExchangeOrders;
+        private System.Windows.Forms.Button btnNewExchangeOrder;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnViewWarehouses;
+        private System.Windows.Forms.Button btnViewProducts;
+        private System.Windows.Forms.Button btnViewUnits;
+        private System.Windows.Forms.Button btnViewProviders;
+        private System.Windows.Forms.Button btnViewCustomers;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
